06118c808004625a2dba8ff7c11a0e2d9a2a8059
Saverio Delpriori <saverio.delpriori@uniurb.it>
2019-09-06 09:00:45 -0400

    Fix reading permission issue

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ADD create-config.py /
 
 WORKDIR /
 
-RUN pip install python-dotenv
+#RUN pip install python-dotenv
 
 CMD [ "python", "./create-config.py", "config/" ]
 
--- create-config.py
+++ create-config.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv
-import os
 from pathlib import Path  # python3 only
 import re
 import sys
@@ -13,6 +11,7 @@ def load_env2var(file_path=""):
             match = envre.match(line)
             if match is not None:
                 result[match.group(1)] = match.group(2)
+
     return result
 
 
@@ -21,15 +20,19 @@ srs_env_path = Path(base_path) / 'srs.env'
 template_env_path = Path(base_path) / 'config.env-template'
 config_env_path = Path(base_path) / 'config.env'
 
-load_dotenv(dotenv_path=srs_env_path, verbose=True)
+srs_env = load_env2var(srs_env_path)
 template_env = load_env2var(template_env_path)
+
+print(srs_env)
+print(template_env)
+
 env_file = open(config_env_path, "w+", newline='\n')
 
 for variable, placeholder in template_env.items():
-        print(variable, ":", placeholder)
-        value = os.getenv(placeholder)
-        value = "" if value is None else value
-
-        env_file.write("{0}={1}\n".format(variable, value))
+    #print(variable, ":", placeholder)
+    value = srs_env.get(placeholder)
+    value = "" if value is None else value
+    env_file.write("{0}={1}\n".format(variable, value))
+    print(variable, ":", value)
 
 env_file.close()
